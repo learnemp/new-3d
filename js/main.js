15ee41519d6ebf8a660237abb8d8698e16244aaf
learnemp <142683021+learnemp@users.noreply.github.com>
2023-09-16 04:15:48 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -277,9 +277,10 @@ function renderAdditionalElements(context) {
   
   context.fillText(headerText, centerX, 90);
 
+
 // Footer
 context.fillStyle = "white"; // Set background color
-context.fillRect(0, window.innerHeight - 100, window.innerWidth, 200); // Draw a background rectangle
+context.fillRect(0, window.innerHeight - 200, window.innerWidth, 200); // Draw a background rectangle
 
 // Footer Text
 const footerText = "I support for a world where deaf people everywhere";
@@ -287,11 +288,13 @@ const footerText2 = "can sign anywhere!";
 const footerTextWidth = context.measureText(footerText).width;
 const footerText2Width = context.measureText(footerText2).width;
 
-const widestTextWidth = Math.max(footerTextWidth, footerText2Width);
-const footerCenterX = (window.innerWidth - widestTextWidth) / 2;
+// Calculate the center for each line of text
+const footerCenterX = (window.innerWidth - footerTextWidth) / 2;
+const footerCenterX2 = (window.innerWidth - footerText2Width) / 2;
 
 context.fillStyle = "orangered"; // Set text color for footer
 context.font = "4em 'Arial'";
-context.fillText(footerText, footerCenterX, window.innerHeight - 120); // Center the footer text horizontally
-context.fillText(footerText2, footerCenterX, window.innerHeight - 60); // Center the second line of footer text horizontally
+context.fillText(footerText, footerCenterX, window.innerHeight - 120); // Center the first line of footer text horizontally
+context.fillText(footerText2, footerCenterX2, window.innerHeight - 60); // Center the second line of footer text horizontally
+
 }
